792566ea8edd5775fe362a69d8ca4949ce7d243e
JBJA <benjamin.soleado@gmail.com>
2022-04-23 16:33:53 -0600

    23/04/2022

--- Assets/Script/ARInteractionsManager.cs
+++ Assets/Script/ARInteractionsManager.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using UnityEngine.EventSystems;
 
 public class ARInteractionsManager : MonoBehaviour
 {
@@ -13,8 +14,13 @@ public class ARInteractionsManager : MonoBehaviour
 
     private GameObject aRPointer;
     private GameObject item3DModel;
+    private GameObject itemSelected;
+
     private bool isInitialPosition;
     private bool isOverUI;
+    private bool isOver3DModel;
+
+    private Vector2 initialTouchPos;
 
     public GameObject Item3DModel
     {
@@ -58,6 +64,7 @@ public class ARInteractionsManager : MonoBehaviour
             {
                 var touchPosition = touchOne.position;
                 isOverUI = isTapOverUI(touchPosition);
+                isOver3DModel = isTapOver3DModel(touchPosition);
             }
 
             if(touchOne.phase == TouchPhase.Moved)
@@ -65,13 +72,54 @@ public class ARInteractionsManager : MonoBehaviour
                 if (aRRaycastManager.Raycast(touchOne.position, hits, TrackableType.Planes))
                 {
                     Pose hitPose = hits[0].pose;
-                    if (!isOverUI)
+                    if (!isOverUI && isOver3DModel)
                     {
                         transform.position = hitPose.position;
                     }
                 }
             }
+
+            // Rotar modelo 3D
+            if (Input.touchCount == 2)
+            {
+                Touch touchTwo = Input.GetTouch(1);
+                if (touchOne.phase == TouchPhase.Began || touchTwo.phase == TouchPhase.Began)
+                {
+                    initialTouchPos = touchTwo.position - touchOne.position;
+                }
+
+                if (touchOne.phase == TouchPhase.Moved || touchTwo.phase == TouchPhase.Moved)
+                {
+                    Vector2 currentTouchPos = touchTwo.position - touchOne.position;
+                    float angle = Vector2.SignedAngle(initialTouchPos, currentTouchPos);
+                    item3DModel.transform.rotation = Quaternion.Euler(0, item3DModel.transform.eulerAngles.y - angle, 0);
+                    initialTouchPos = currentTouchPos;
+                }
+            }
+            if (isOver3DModel && item3DModel == null && !isOverUI)
+            {
+                GameManager.instance.ARPosition();
+                item3DModel = itemSelected;
+                itemSelected = null;
+                aRPointer.SetActive(true);
+                transform.position = item3DModel.transform.position;
+                item3DModel.transform.parent = aRPointer.transform;
+            }
         }
+    }
+
+    private bool isTapOver3DModel(Vector2 touchPosition)
+    {
+        Ray ray = aRCamera.ScreenPointToRay(touchPosition);
+        if (Physics.Raycast(ray, out RaycastHit hit3DModel))
+        {
+            if (hit3DModel.collider.CompareTag("Item"))
+            {
+                itemSelected = hit3DModel.transform.gameObject;
+                return true;
+            }
+        }
+        return false;
     }
 
     private bool isTapOverUI(Vector2 touchPosition)
